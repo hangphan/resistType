59a201bc40176d054fde7baa9af9284f1399f4d4
hangphan <hangphan@well.ox.ac.uk>
2017-05-12 09:37:05 +0000

    removing irrelevant lines from makeStart.sh

--- src/makeStart.sh
+++ src/makeStart.sh
@@ -7,10 +7,6 @@ mkdir -p $studyName/fastq $studyName/resistType $studyName/ErrFiles $studyName/t
 echo "#!/bin/bash" >$studyName/run_resistType.sh
 echo "#$ -cwd" >>$studyName/run_resistType.sh
 echo "#$ -V" >>$studyName/run_resistType.sh
-echo "#$ -P bag.prjb -q short.qb ">>$studyName/run_resistType.sh
-echo "#$ -pe shmem 1 " >>$studyName/run_resistType.sh
-echo "#$ -e $PWD/ErrFiles/" >>$studyName/run_resistType.sh
-echo "#$ -o $PWD/ErrFiles/" >>$studyName/run_resistType.sh
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo "SCRIPTDIR=$SCRIPTDIR" >>$studyName/run_resistType.sh
 cat $SCRIPTDIR/runResistTypeBody.sh >>$studyName/run_resistType.sh
