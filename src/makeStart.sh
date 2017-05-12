54b0b1baa66205f73ebc641bd2ba65d91d698df4
hangphan <hangphan@well.ox.ac.uk>
2017-05-12 09:39:40 +0000

    removing redundant files

---
+++ src/makeStart.sh
@@ -0,0 +1,34 @@
+/well/bag/hangphan/resistType/src
+-bash-4.1$ cat makeStart.sh
+#!/bin/bash
+#Please change the header here to suit your cluster job submission system
+studyName=$1
+mkdir -p  $studyName
+
+mkdir -p $studyName/fastq $studyName/resistType $studyName/ErrFiles $studyName/tmpDir $studyName/spadesOutput $studyName/filteredFastq
+
+echo -e $"#!/bin/bash\n#$ -cwd\n#$ -V" >$studyName/run_resistType.sh
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo "SCRIPTDIR=$SCRIPTDIR" >>$studyName/run_resistType.sh
+cat $SCRIPTDIR/runResistTypeBody.sh >>$studyName/run_resistType.sh
+cp $SCRIPTDIR/runBbduk.sh $studyName
+
+echo -e $"#!/bin/bash\n#$ -cwd\n#$ -V" >$studyName/runMLST.sh
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo "SCRIPTDIR=$SCRIPTDIR" >>$studyName/runMLST.sh
+echo "python $SCRIPTDIR/mlstTyping.py -s $sampleid -c spadesOutput/$sampleid/conetigs.fasta" >>$studyName/runMLST.sh
+
+echo -e $"#!/bin/bash\n#$ -cwd\n#$ -V" >$studyName/runISFinder.sh
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo "SCRIPTDIR=$SCRIPTDIR" >>$studyName/runISFinder.sh
+echo "python $SCRIPTDIR/ISFinder.py -s $sampleid -c spadesOutput/$sampleid/conetigs.fasta" >>$studyName/runISFinder.sh
+
+echo -e $"#!/bin/bash\n#$ -cwd\n#$ -V" >$studyName/runPlasmidFinder.sh
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo "SCRIPTDIR=$SCRIPTDIR" >>$studyName/runPlasmidFinder.sh
+echo "python $SCRIPTDIR/plasmidFinder.py -s $sampleid -c spadesOutput/$sampleid/conetigs.fasta" >>$studyName/runPlasmidFinder.sh
+
+
+
+cp $SCRIPTDIR/submitJobs.sh $studyName
+
